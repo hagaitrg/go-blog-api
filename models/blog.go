69776519d39b6167c430ef00c2b545d39ea7d9b0
hagaitrg <disahagait@gmail.com>
2022-10-15 08:21:34 +0700

    setup blog models

--- models/blog.go
+++ models/blog.go
@@ -4,4 +4,11 @@ import (
 	"time"
 )
 
-type 
+type Blog struct {
+	ID 			uint 		`json:"id" gorm:"primary_key"`
+	title 		string		`json:"title gorm: "type:varchar(120)""`
+	content 	string		`json:"content" gorm: "type:varchar(500)"`
+	slug		string 		`json:"slug" gorm: "type:varchar(500)"`
+	CreatedAt	time.Time 	`gorm:"default:CURRENT_TIMESTAMP" json:"created_at`
+	UpdatedAt	time.Time 	`gorm:"default:CURRENT_TIMESTAMP" json:"updated_at`
+}
